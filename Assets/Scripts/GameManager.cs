03f7a1a05ed2125c7663ac02290e82b3bb9e7caa
ThugUnicorn713 <victoriaabeale@gmail.com>
2024-11-04 15:59:57 +0000

    we got cards in our hand!
    we have a hand at the beginning of the game....but hey! thats something!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,4 +7,40 @@ public class GameManager : MonoBehaviour
     public List<GameObject> deck = new List<GameObject>();
     public Transform[] playerSlots;
     public bool[] emptySlots;
+
+    public void Start() 
+    { 
+        for (int i = 0; i < emptySlots.Length; ++i)
+        {
+            if (emptySlots[i] && deck.Count > 0)
+            {
+                DrawCard();
+            }
+        }
+        
+    }
+
+    public void DrawCard()
+    {
+       if (deck.Count >= 1)
+       {
+            GameObject ranCard = deck[Random.Range(0, deck.Count)];
+
+            for (int i = 0; i < emptySlots.Length; i++)
+            {
+                if (emptySlots[i] == true)
+                {
+                    ranCard.SetActive(true);
+                    ranCard.transform.SetParent(null);
+                    ranCard.transform.rotation = Quaternion.Euler(-90, 0, 0);
+                    ranCard.transform.position = playerSlots[i].position;
+                    //Debug.Log("Positioning card at: " + playerSlots[i].position);
+                    emptySlots[i] = false;
+                    deck.Remove(ranCard);
+                    return;
+                }
+            }
+       }
+    }
+
 }
